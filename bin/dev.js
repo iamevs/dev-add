667033538cd6e736301de240e37e3f908d9fe8d0
iamevs <iamevs143@gmail.com>
2025-05-16 06:40:56 +0530

    Added : No Component Function - to skip the updates in index.js

--- bin/dev.js
+++ bin/dev.js
@@ -10,9 +10,11 @@ program
   .option('--template <template>', 'Template to use (e.g., react-component)')
   .option('--named', 'Use named export')
   .option('--dry', 'Dry run (no files written)')
+  .option('--verbose', 'Verbose logging')
+  .option('--nocomp', 'Do not create or update index.js')
   .description('Add a new component or utility file')
   .action((filePath, options) => {
-    addFiles(filePath, options); // ✅ Pass options here
+    addFiles(filePath, options);
   });
 
 program.parse(process.argv);
